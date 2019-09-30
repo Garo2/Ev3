8593a1be2ea3205cd6d1d41950ddd49c247eb8e8
MadJacksson <55533084+MadJacksson@users.noreply.github.com>
2019-09-30 10:13:12 +0200

    test kod
    test

--- test arm/src/test arm.c
+++ test arm/src/test arm.c
@@ -4,7 +4,6 @@
  \date		${date}
  \brief		Simple Hello World! for the Ev3
 */
-
 #include <ev3.h>
 
 int main(void)
@@ -14,10 +13,31 @@ int main(void)
 	InitEV3();
 
 	//TODO Place here your code
-	if(IN_1 == 1){
-	LcdPrintf(1, "Hello World!\n");
-	Wait(SEC_2);
-	}
+	setAllSensorMode(TOUCH_PRESS, NO_SEN, TOUCH_PRESS, US_DIST_MM);
+	readSensor(IN_1);
+	readSensor(IN_3);
+		if(readSensor(IN_1) == 1)
+		{
+			LcdPrintf(1, "FIRST IS PRESSED!\n");
+			Wait(SEC_2);
+		}
+
+		if(readSensor(IN_3) == 1)
+		{
+			LcdPrintf(1, "SECOND IS PRESSED!\n");
+			Wait(SEC_2);
+		}
+
+		else {
+				LcdPrintf(1, "NOPE!\n");
+				Wait(SEC_2);
+		}
+
+		if(readSensor(IN_3)+readSensor(IN_1)==1)
+		{
+			LcdPrintf(1, "BOTH ARE PRESSED!\n");
+			Wait(SEC_2);
+		}
 
 	FreeEV3();
 	return 0;
